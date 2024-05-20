e193c68100b641b99f939a47ce180cff6ee2c640
KevinGil12C <kebo.jcg77@gmail.com>
2024-05-20 11:13:25 -0600

    url raw

--- assets/data/academic-data.js
+++ assets/data/academic-data.js
@@ -1,31 +1,31 @@
 export const academicData = [
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/uaemex.png",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/uaemex.png?raw=true",
         title: "Ingeniería en Computación",
         date: "En curso - UAEMEX",
     },
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/alura_logo.png",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/alura_logo.png?raw=true",
         title: "Formación Front End G6 - ONE",
         date: "En curso - Alura",
     },
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png?raw=true",
         title: "Análisis de datos",
         date: "2024 - Google",
     },
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/aprende_icon.png",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/aprende_icon.png?raw=true",
         title: "Especialidad en Tratemiento de datos",
         date: "2024 - Capacitate para el empleo",
     },
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/red_por_ciberseguridad_icon.jpg",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/red_por_ciberseguridad_icon.jpg?raw=true",
         title: "Operación Aleph: SC-900",
         date: "2024 - Red por la ciberseguridad",
     },
     {
-        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png",
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png?raw=true",
         title: "Ciberseguridad",
         date: "En curso - Google",
     },
