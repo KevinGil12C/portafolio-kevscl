b2506ede5b08255f98667e28122a9662a1b8238c
KevinGil12C <kebo.jcg77@gmail.com>
2024-05-20 09:58:51 -0600

    academic

---
+++ assets/data/academic-data.js
@@ -0,0 +1,33 @@
+export const academicData = [
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/uaemex.png",
+        title: "Ingeniería en Computación",
+        date: "En curso - UAEMEX",
+    },
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/alura_logo.png",
+        title: "Formación Front End G6 - ONE",
+        date: "En curso - Alura",
+    },
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png",
+        title: "Análisis de datos",
+        date: "2024 - Google",
+    },
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/aprende_icon.png",
+        title: "Especialidad en Tratemiento de datos",
+        date: "2024 - Capacitate para el empleo",
+    },
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/red_por_ciberseguridad_icon.jpg",
+        title: "Operación Aleph: SC-900",
+        date: "2024 - Red por la ciberseguridad",
+    },
+    {
+        img: "https://github.com/KevinGil12C/portafolio-kevscl/blob/main/assets/academic/google_icon.png",
+        title: "Ciberseguridad",
+        date: "En curso - Google",
+    },
+    
+]
--- modules.js
+++ modules.js
@@ -5,3 +5,5 @@ import { card__container } from "./assets/controllers/skillsCards.js";
 import {portafolioContainer } from "./assets/controllers/portafolioCards.js"
 
 import menuActive from "./assets/controllers/header_activate.js";
+
+import { academicContainer } from "./assets/controllers/academicCards.js";
