97e1791fbe862de44afbc7b2cf4f71991d82e661
KevinGil12C <kebo.jcg77@gmail.com>
2024-08-24 18:01:50 -0600

    Agregando carrucel

--- assets/controllers/skillsCards.js
+++ assets/controllers/skillsCards.js
@@ -1,12 +1,12 @@
 import skillData from "../data/skills-data.js";
 
-export const card__container = document.querySelector('.skills__container');
+export const card__container = document.querySelector('.slide__track');
 
 const cardSkill =  skillData.map(
     (card) => `
-    <div class="card">
+    <div class="slide">
         <iconify-icon class="card__icon" icon="${card.icon}"></iconify-icon>
-        <h3 class="card__description">${card.title}</h3>
+        <h3 class="tag">${card.title}</h3>
     </div>
 `).join(" ");
 
